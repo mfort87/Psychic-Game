7bdc052259484c357827ce3ba6f5ac633e2d006c
Matt Fort <mfort8487@gmail.com>
2018-06-18 16:01:46 -0500

    second save

---
+++ assets/javascript/game.js
@@ -0,0 +1,11 @@
+var CChoice = [a, b, c, d, e, f, g, h, i, j, k, l, m, n, o, p, q, r, s, t, u, v, w, x, y, z];
+
+var wins = 0;
+var losses = 0;
+var guesses = 12;
+var guessCount = [];
+var setComputerPick = "";
+
+document.onkeyup = function(event){
+    var userPick = String.fromCharCode(event.keyCode);
+}
